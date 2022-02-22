f0bad57bbdd65f8ed43c2c931a1a8a36d92f44b1
Gurpreetw <preetgur.gsr@gmail.com>
2022-02-22 21:54:38 +0530

    edit sh fike

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 echo "pipeline name"
 read ppln_name
 
-echo "enter relo name"
+echo "enter repo name"
 read repo_name
 az pipelines create --name $ppln_name --description 'Pipeline for contoso project' --repository $repo_name --branch master --repository-type tfsgit --yml-path azure-pipelines.yml
